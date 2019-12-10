0169da6d693e7f5d2a9f4923d717c38f15f745ee
takanakahiko <takanakahiko@gmail.com>
2019-12-11 02:55:19 +0900

    ルール修正とレイアウト微修正

--- src/modules/App.vue
+++ src/modules/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="container">
-    <a class="big-button" href="#" @click="increment">
+    <div class="content">
+      <a class="big-button" href="#" @click="increment">
       <svg class="likeButton" viewBox="0 0 500 500">
         <path class="heart" d="M250,187.4c-31.8-47.8-95.5-19.8-95.5,32.2c0,35.2,31.8,60.3,55.7,79.2c24.9,19.7,31.8,23.9,39.8,31.8 c7.9-7.9,14.6-12.6,39.8-31.8c24.3-18.5,55.7-44.4,55.7-79.6C345.5,167.6,281.8,139.7,250,187.4z"/>
       </svg>
       <i class="fa fa-share"></i>
     </a>
     <p class="counter">{{count}}いいね</p>
-    <div class="note">
-      メンテナンス用(最初にカウンタを初期化するやつ)→
-      <input type="button" value="create" @click="createCounter">
-    </div>
     <div class="credit">presented by Prickathon</div>
+    </div>
   </div>
 </template>
 
@@ -29,7 +27,7 @@ export default Vue.extend({
     };
   },
   mounted() {
-    db.collection('counters').onSnapshot(snapshot => {
+    db.collection('counter').onSnapshot(snapshot => {
       let total_count = 0;
       snapshot.forEach(doc => total_count += doc.data().count );
       this.count =  total_count;
@@ -38,16 +36,8 @@ export default Vue.extend({
   methods: {
     increment() {
       const shard_id = Math.floor(Math.random() * num_shards).toString();
-      const shard_ref = db.collection('counters').doc(shard_id);
+      const shard_ref = db.collection('counter').doc(shard_id);
       return shard_ref.update("count", firebase.firestore.FieldValue.increment(1));
-    },
-    createCounter() { // メンテナンス用(最初にカウンタを初期化するやつ)
-      var batch = db.batch();
-      for (let i = 0; i < num_shards; i++) {
-          let shardRef = db.collection('counters').doc(i.toString());
-          batch.set(shardRef, { count: 0 });
-      }
-      return batch.commit();
     }
   }
 });
@@ -60,20 +50,18 @@ body {
 
 .container {
   font-family: 'ヒラギノ角ゴ StdN','Hiragino Kaku Gothic StdN',sans-serif;
-  max-width: 500px;
   height: 70vh;
   margin: 0 auto;
   margin-top: 15vh;
   text-align: center;
   color: White;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .counter{
   font-size: 40px;
-}
-
-.note {
-  font: 10px gray;
 }
 
 .credit{
@@ -83,8 +71,8 @@ body {
 .big-button {
   margin: 0 auto;
   display: block;
-  width: 250px;
-  height: 250px;
+  width: 200px;
+  height: 200px;
   background:white;
   text-align: center;
   border-radius: 50%;
